1019231f070f19a6aada7ee8cd9b459e69379346
isabellespierenburg <39111698+isabellespierenburg@users.noreply.github.com>
2018-05-17 11:35:30 +0200

    functie toegevoegd

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,5 +20,18 @@ class DefaultController extends Controller
         ]);
     }
 
+    class DefaultController extends Controller
+    {
+        /**
+         * @Route("/", name="homepage")
+         */
+        public function indexAction(Request $request)
+        {
+            // replace this example code with whatever you need
+            return $this->render('default/index.html.twig', [
+                'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            ]);
+        }
+
 
 }
